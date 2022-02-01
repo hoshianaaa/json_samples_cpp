94b540a637faa57a8d6f401be9b5b8b482d3b850
hoshianaaa <hoshina54321@gmail.com>
2022-02-01 17:19:02 +0900

    読み込みしてparseして表示できた

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-int main(int argc, const char * argv[])
-{
-    return 0;
-}
+#include <fstream>
+#include <iostream>
 
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+
+using namespace rapidjson;
+
+int main(int, char **)
+{
+    std::ifstream ifs("sample.json");
+    IStreamWrapper isw(ifs);
+
+    Document doc;
+    doc.ParseStream(isw);
+
+    std::cout << "a: " << doc["a"].GetInt() << std::endl;
+}
